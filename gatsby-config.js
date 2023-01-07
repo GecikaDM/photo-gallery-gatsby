b21b296e59da3ea38c5ab840b0565b35b8c0af68
Gécika DM <gecikadm@macbook-pro-de-gecika.home>
2023-01-07 19:06:32 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Photo-Gallery`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
