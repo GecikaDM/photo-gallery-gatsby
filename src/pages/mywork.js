fe894d3a35948b1ddb93543d3bc4701b085bb7c3
Gécika DM <gecikadm@macbook-pro-de-gecika.home>
2023-01-08 15:49:44 +0100

    practice structure

---
+++ src/pages/mywork.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+
+const MyWork = () => {
+    return (
+        <Layout pageTitle="My Work">
+        <p>Hi there! Have a look at my Work.</p>
+        </Layout>
+
+    )
+}
+
+export const Head = () => <title> My Work</title>
+
+
+export default MyWork;
